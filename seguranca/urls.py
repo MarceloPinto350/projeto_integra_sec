46e3df37a9f978d248b389326400db10b9cf1923
Marcelo Pinto <pintomarc@gmail.com>
2024-05-11 13:41:50 -0300

    criando os modelos genéricos de api

--- seguranca/urls.py
+++ seguranca/urls.py
@@ -1,10 +1,13 @@
 from django.urls import path
 
-from .views import  TipoAplicacaoAPIView, AplicacaoAPIView
+from .views import  TipoAplicacaoAPIView, TiposAplicacaoAPIView, AplicacaoAPIView, AplicacoesAPIView, ResultadoScanAPIView, ResultadosScanAPIView
 
 # define as rotas para as APIs
 urlpatterns = [
-      path('tiposaplicacao/', TipoAplicacaoAPIView.as_view(), name='tiposaplicacao'),
-      path('aplicacoes/', AplicacaoAPIView.as_view(), name='aplicacoes'),
-      #path('scan/', ScanView.as_view(), name='scan'),
+      path('tiposaplicacao/', TiposAplicacaoAPIView.as_view(), name='tiposaplicacao'),
+      path('aplicacoes/', AplicacoesAPIView.as_view(), name='aplicacoes'),
+      path('resultadosscan/', ResultadosScanAPIView.as_view(), name='resultadosscan'),
+      path('tiposaplicacao/<int:pk>/', TipoAplicacaoAPIView.as_view(), name='tipoaplicacao'),
+      path('aplicacoes/<int:pk>/', AplicacaoAPIView.as_view(), name='aplicacao'),
+      path('resultadosscan/<int:pk>/', ResultadoScanAPIView.as_view(), name='resultadoscan'),
 ]
--- seguranca/views.py
+++ seguranca/views.py
@@ -1,79 +1,38 @@
-from rest_framework.views import APIView
-from rest_framework.response import Response
-from rest_framework import status
+from rest_framework import generics 
 
-from .models import TipoAplicacao,Aplicacao, ResultadoScan
-from .serializers import TipoAplicacaoSerializer,AplicacaoSerializer, ResultadoScanSerializer
-#from .scanners import sonarQube #, owaspZap, owaspDependencyCheck
+from .models import TipoAplicacao, Aplicacao, ResultadoScan
+from .serializers import TipoAplicacaoSerializer, AplicacaoSerializer, ResultadoScanSerializer
 
-# Importações das bibliotecas para as ferramentas de segurança
-#import sonarQube
-#import owaspZap
-#import owaspDependencyCheck
+# classe genérica para listar e criar novos registros de tipos de aplicação
+# generics.ListCreateAPIView - lista e cria novos registros
+# generics.RetrieveUpdateDestroyAPIView - recupera, atualiza e deleta um registro registro   
+# para mais informações sobre as classes genéricas do Django Rest Framework, acesse: https://www.django-rest-framework.org/api-guide/generics/
+class TiposAplicacaoAPIView(generics.ListCreateAPIView):
+   queryset = TipoAplicacao.objects.all()
+   serializer_class = TipoAplicacaoSerializer
 
-class TipoAplicacaoAPIView(APIView):
-   """
-   API da Classe TipoAplicacao para obter e criar tipos de aplicação
-   """
-   def get(self, request):
-      #print(request.user) # visualiza o usuário que fez a requisição ...
-      print(dir(request)) # visualiza todos os dados do request
-      tiposaplicacao = TipoAplicacao.objects.all()
-      serializer= TipoAplicacaoSerializer(tiposaplicacao, many=True)
-      return Response(serializer.data)
-   
-   def post(self, request):
-      serializer = TipoAplicacaoSerializer(data=request.data)
-      if serializer.is_valid():
-         serializer.save()
-         return Response(serializer.data, status=status.HTTP_201_CREATED)
-      return Response(serializer.errors, status=status.HTTP_400_BAD_REQUEST)
-      
 
-class AplicacaoAPIView(APIView):
-   """
-   API da Classe Aplicação para obter e criar aplicações
-   """
-   def get(self, request):
-      aplicacoes = Aplicacao.objects.all()    
-      serializer = AplicacaoSerializer(aplicacoes, many=True)
-      return Response(serializer.data)
-      
-   def post(self, request):
-      serializer = AplicacaoSerializer(data=request.data)
-      if serializer.is_valid():
-         serializer.save()
-         return Response(serializer.data,status=status.HTTP_201_CREATED)
-      else:
-         return Response(serializer.errors,status=status.HTTP_400_BAD_REQUEST)
+
+class TipoAplicacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+   queryset = TipoAplicacao.objects.all()
+   serializer_class = TipoAplicacaoSerializer
+
+
+class AplicacoesAPIView(generics.ListCreateAPIView):
+   queryset =  Aplicacao.objects.all()
+   serializer_class = AplicacaoSerializer
 
       
-class ResultadoScanAPIView(APIView):
-   """
-   API para obter os resultados da varredura de segurança
-   """
-   def post(self, request):
-      id_aplicacao = request.data['id_aplicacao']
-      aplicacao = Aplicacao.objects.get(pk=id_aplicacao)
-      # Executar varredura SAST com SonarQube
-      sonar_result = sonarQube.scan(aplicacao.url_acesso, aplicacao.url_fonte)
-      # Executar varredura DAST com OWASP ZAP
-#      owaspZap_result = owaspZap.scan(aplicacao.url_acesso)
-      # Executar varredura SCA com OWASP Dependency Check
-#      owaspDC_result = owaspDependencyCheck.scan(aplicacao.url_fonte)
-      # opção para combinar os rsultados em um único objeto
-#      scan_result#         'sonarQube':sonar_result,
-#         'owaspZap':owaspZap_result,
-#         'owaspDependencyCheck':owaspDC_result
-#      }
-      # salvar o resultado da varredura no banco de dados
-      resultado_scan = ResultadoScan.object.create (aplicacao=aplicacao, resultado=scan_result)
-      # serializar o resultado da varredura
-#      serializer = ScanResultSerializer(resultado_scan) 
-#      if serializer.is_valid():
-#         serializer.save()
-#         return Response(serializer.data)
-#      else:
-#         return Response(serializer.errors)
+class AplicacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
+   queryset =  Aplicacao.objects.all()
+   serializer_class = AplicacaoSerializer
 
 
+class ResultadosScanAPIView(generics.ListCreateAPIView):
+   queryset = ResultadoScan.objects.all()
+   serializer_class = ResultadoScanSerializer
+
+
+class ResultadoScanAPIView(generics.RetrieveUpdateDestroyAPIView):
+   queryset = ResultadoScan.objects.all()
+   serializer_class = ResultadoScanSerializer
