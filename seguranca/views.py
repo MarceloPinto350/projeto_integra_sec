3efc2220758adf6d42129c017c09806f9ba6650c
Marcelo Pinto <pintomarc@gmail.com>
2024-05-11 13:44:59 -0300

    ajuste nos comentários

--- seguranca/views.py
+++ seguranca/views.py
@@ -6,33 +6,34 @@ from .serializers import TipoAplicacaoSerializer, AplicacaoSerializer, Resultado
 # classe genérica para listar e criar novos registros de tipos de aplicação
 # generics.ListCreateAPIView - lista e cria novos registros
 # generics.RetrieveUpdateDestroyAPIView - recupera, atualiza e deleta um registro registro   
-# para mais informações sobre as classes genéricas do Django Rest Framework, acesse: https://www.django-rest-framework.org/api-guide/generics/
+# para listar e criar novos registros de tipos de aplicação 
 class TiposAplicacaoAPIView(generics.ListCreateAPIView):
    queryset = TipoAplicacao.objects.all()
    serializer_class = TipoAplicacaoSerializer
 
-
-
+# para recuperar, atualizar e deletar um registro de tipo de aplicação
 class TipoAplicacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
    queryset = TipoAplicacao.objects.all()
    serializer_class = TipoAplicacaoSerializer
 
 
+# para listar e criar novos registros de aplicações
 class AplicacoesAPIView(generics.ListCreateAPIView):
    queryset =  Aplicacao.objects.all()
    serializer_class = AplicacaoSerializer
 
-      
+# para recuperar, atualizar e deletar um registro de aplicação      
 class AplicacaoAPIView(generics.RetrieveUpdateDestroyAPIView):
    queryset =  Aplicacao.objects.all()
    serializer_class = AplicacaoSerializer
 
 
+# para listar e criar novos registros de resultados de scan
 class ResultadosScanAPIView(generics.ListCreateAPIView):
    queryset = ResultadoScan.objects.all()
    serializer_class = ResultadoScanSerializer
 
-
+# para recuperar, atualizar e deletar um registro de resultado de scan
 class ResultadoScanAPIView(generics.RetrieveUpdateDestroyAPIView):
    queryset = ResultadoScan.objects.all()
    serializer_class = ResultadoScanSerializer
