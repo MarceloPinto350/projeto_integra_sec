d45626ab44c9fa9a2402910b2d1fd9fca7afcc07
Marcelo Pinto <pintomarc@gmail.com>
2024-12-22 02:13:17 -0300

    CONFIGURAÇÃO DE REDE PARA EXPOR PORTAS CORRETAS

--- integracao/containernet/config_topologia.py
+++ integracao/containernet/config_topologia.py
@@ -26,7 +26,12 @@ def topologia():
 	# Criar o container do SonarQube
    sonar = net.addDocker('sonar',
       ip='10.100.0.120', 
-      cpu_shares=20, privileged=True,
+      ipbase='172.17.0.0/24',
+      port='9000:9000',
+      port_bindings={9000:9000},
+      publish_all_ports=True,
+      cpu_shares=20, 
+      privileged=True,
       environment={'DISPLAY':":0"},
       volumes=["/tmp/.X11-unix:/tmp/.X11-unix:rw",
          "sonar_data:/opt/sonarqube/data",
@@ -37,15 +42,20 @@ def topologia():
    # Criar o container do sonar em modo CLI
    sonar_cli = net.addDocker('sonar_cli',
       ip='10.100.0.125',
-      cpu_shares=20, privileged=True,
+      ipbase='172.17.0.0/24',
+      cpu_shares=20, 
+      privileged=True,
       environment={'DISPLAY':":0"},
       volumes=["/tmp/.X11-unix:/tmp/.X11-unix:rw","app:/app"],
-      dimage='ramonfontes/ubuntu:trusty')
+      #dimage='ramonfontes/ubuntu:trusty')
+      dimage='marcelopinto350/sonarcli-ubuntu:trusty')
 
    # Criar o container do OWASP ZAP
    owasp_zap = net.addDocker('owasp_zap', 
       ip='10.100.0.130',
-      cpu_shares=20, privileged=True,
+      ipbase='172.17.0.0/24',
+      cpu_shares=20, 
+      privileged=True,
       environment={'DISPLAY':":0"},
       #dimage="ramonfontes/zaproxy",		#atualizado o uso para essa versão por conta de ter mais recursos
       dimage="marcelopinto350/owasp_zap",
@@ -55,7 +65,9 @@ def topologia():
    # Criar o container do Owasp dependency-check
    owasp_dc = net.addDocker('owasp_dc',
       ip='10.100.0.135',
-      cpu_shares=20, privileged=True,
+      ipbase='172.17.0.0/24',
+      cpu_shares=20, 
+      privileged=True,
       environment={'DISPLAY':":0"},
       #dimage="marcelopinto350/owasp-dependency-check:9.2",
       dimage="marcelopinto350/owasp_dc:9.2",
@@ -64,15 +76,24 @@ def topologia():
 
    # Criar o container do BD da aplicação de teste DVWA
    dvwa_db = net.addDocker('dvwa_db',
-      ip='10.100.0.140', privileged=True,
+      ip='10.100.0.140', 
+      ipbase='172.17.0.0/24',
       dimage="ramonfontes/mariadb:11",
-      environment={'DISPLAY':":0",'MYSQL_ROOT_PASSWORD':'dvwa','MYSQL_DATABASE':'dvwa','MYSQL_USER':'dvwa','MYSQL_PASSWORD':'p@ssw0rd'}, 
+      environment={'DISPLAY':":0",
+            'MYSQL_ROOT_PASSWORD':'dvwa',
+            'MYSQL_DATABASE':'dvwa',
+            'MYSQL_USER':'dvwa',
+            'MYSQL_PASSWORD':'p@ssw0rd'}, 
       volumes=["/tmp/.X11-unix:/tmp/.X11-unix:rw","dvwa_db:/var/lib/mysql"])
    
    # Criar o container da aplicação de teste DVWA
    dvwa = net.addDocker('dvwa',
       ip='10.100.0.145',
-      port='4280:80', privileged=True,
+      ipbase='172.17.0.0/24',
+      port='4280:80', 
+      port_bindings={80:4280},
+      publish_all_ports=True,
+      privileged=True,
       cpu_shares=20,
       #dimage="ramonfontes/dvwa:latest",
       #dimage="marcelopinto350/dvwa:latest",
@@ -83,19 +104,24 @@ def topologia():
    # Criar o BD da aplicação de segurança APPSEG
    appseg_db = net.addDocker('appseg_db',
       ip='10.100.0.150',
+      ipbase='172.17.0.0/24',
+      port='5432:5432',
+      port_bindings={5432:5432},
+      publish_all_ports=True,
       #dimage="ramonfontes/postgres:alpine", privileged=True,
-      dimage="marcelopinto350/postgres:alpine", privileged=True,
+      dimage="marcelopinto350/postgres:alpine", 
+      privileged=True,
       environment={'DISPLAY':":0"},
-         # 'POSTGRES_DB':'appseg',
-         # 'POSTGRES_USER':'postgres',
-         # 'POSTGRES_PASSWORD':'postgres',
-         # 'POSTGRES_PORT':'5432'},
       volumes=["/tmp/.X11-unix:/tmp/.X11-unix:rw","pg_data:/var/lib/postgresql/data"])
    
    # Criar a aplicação de segurança APPSEG
    appseg = net.addDocker('appseg',
       ip='10.100.0.155',
-      port='8000:8000', privileged=True,
+      ipbase='172.17.0.0/24',
+      port='8000:8000', 
+      port_bindings={8000:8000},
+      publish_all_ports=True,
+      privileged=True,
       cpu_shares=20,
       #dimage="ramonfontes/python:3.10",
       dimage="marcelopinto350/appseg:beta",
@@ -163,6 +189,9 @@ def topologia():
    
    # Subir a aplicação AppSeg
    appseg.cmd('cd /appseg & python3 manage.py runserver 0.0.0.0:8000 &')
+   
+   # configuração específica para o permitir aesso às aplicaçãoes via host local do docker
+   appseg.cmd("route add -net 172.17.0.0 netmask 255.255.0.0 gw 172.17.0.1 "
    
  
    info('*** Executando CLI\n')
